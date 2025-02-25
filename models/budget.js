c102ace3e4f2229b2975bc3224bc3c188a2d4190
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-02-25 10:17:06 -0600

    Budget Schema

---
+++ models/budget.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const BudgetSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    category: { type: String, required: true }, // e.g., Food, Rent
+    limit: { type: Number, required: true }, // Budget limit
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true }
+  });
+  
+  module.exports = mongoose.model("Budget", BudgetSchema);
