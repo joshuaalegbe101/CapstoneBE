ce3b08b65fdc55342f279acfe5915d899557613e
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-02-25 11:35:35 -0600

    serverc config

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+//imports
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-
+//Setup
 const app = express();
 dotenv.config();
 let PORT = process.env.PORT || 3001;
+
+//Middleware
+app.use(express.json())
+
+//Routes
+
+
+
+
+
 
 
 //Listen
